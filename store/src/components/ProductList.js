e0e0f3ce9e2c1a85abe879df19cc1a332526c49b
randy <ndokongrandy@yahoo.com>
2020-10-07 05:07:10 +0100

    i didnot even finish

--- store/src/components/ProductList.js
+++ store/src/components/ProductList.js
@@ -82,7 +82,7 @@ const ProductList = () =>{
                                             })
                                         }
     
-                                    }
+                                    }()
                                }
                             </div>
                         </div>
